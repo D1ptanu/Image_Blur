eedfb072a0500d48da5e9faea20e234374839ded
D1ptanu <89304034+D1ptanu@users.noreply.github.com>
2022-08-02 14:23:45 +0530

    Create blur.m

---
+++ blur.m
@@ -0,0 +1,32 @@
+function blurX( img, w)           
+[a,b]= size(img);
+out= zeros( a, b,'uint8');          % preallocation for faster matrix operation
+for aa= 1:a
+    for bb= 1:b
+        i= subarray(aa,bb,w,a,b);
+        sa = img(i(1):i(2),i(3):i(4));
+        [m,n]= size(sa);
+        t= sum(sum(sa));
+        avg= uint8(t/(m*n));
+        out(aa,bb)= avg; 
+    end
+end
+imshow(out)
+imwrite(out,'blur.png');
+        
+function indices= subarray(x,y,w,a,b)  %returns the indices of the submatrix
+xl= x-w;
+xu= x+w;
+yl= y-w;
+yu= y+w;
+if xl<1 
+    xl= 1;
+elseif xu>a
+    xu=a;
+end
+if yl<1
+    yl=1;
+elseif yu>b
+    yu=b;
+end
+indices = [xl xu yl yu];
